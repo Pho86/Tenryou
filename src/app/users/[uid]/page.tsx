8a4c0c2abc89ce9c0bd617827bf7b6ab15818d00
Pho86 <philip03ho@gmail.com>
2024-05-05 20:57:33 -0700

    swap to enkanetwork test

--- src/app/users/[uid]/page.tsx
+++ src/app/users/[uid]/page.tsx
@@ -16,49 +16,52 @@ export default function UIDPage({ params }: { params: { uid: string } }) {
 
     useLayoutEffect(() => {
         axios.get(`/api/player/${params.uid}`)
+        // axios.get(`https://tenryou.vercel.app/api/player/${params.uid}`)
             .then(response => {
                 response.data.characters.forEach((character: any) => {
                     addFileName([character]);
                 })
                 setPlayerData(response.data);
+                console.log(response.data)
                 setLoading(false);
                 document.title = `${response.data.player.username} - Tenryou 💮`;
             })
             .catch(error => {
+                console.log(error)
                 setLoading(false);
                 setError(true);
             });
     }, [params.uid]);
 
-    if (loading) {
-        return <>
-            <NavBar active={2} />
-            <main className="flex flex-col md:pt-16 px-8 justify-center items-center relative h-screen">
-                <div className="w-full max-w-screen-2xl">
-                    <Loader />
-                </div>
-                <Footer className="absolute bottom-0" />
-            </main>
-        </>;
-    }
+    // if (loading) {
+    //     return <>
+    //         <NavBar active={2} />
+    //         <main className="flex flex-col md:pt-16 px-8 justify-center items-center relative h-screen">
+    //             <div className="w-full max-w-screen-2xl">
+    //                 <Loader />
+    //             </div>
+    //             <Footer className="absolute bottom-0" />
+    //         </main>
+    //     </>;
+    // }
 
-    if (error || !playerData || !playerData.characters || playerData.characters.length === 0) {
-        return (
-            <>
-                <NavBar active={2} />
-                <main className="flex h-screen flex-col gap-10 items-center justify-center relative">
-                    <h1 className="text-9xl">404</h1>
-                    <p className="text-2xl">Oops... Something went wrong, maybe you inputted the wrong UID, or the account was not public.</p>
-                    <Link href="/" className="">
-                        <button className="border-text border-2 hover:bg-bg-dark transition-all px-4 p-2 rounded-xl">Back To Home</button>
-                    </Link>
-                    <div className="absolute bottom-0 w-full">
-                        <Footer />
-                    </div>
-                </main>
-            </>
-        );
-    }
+    // if (error || !playerData || !playerData.characters || playerData.characters.length === 0) {
+    //     return (
+    //         <>
+    //             <NavBar active={2} />
+    //             <main className="flex h-screen flex-col gap-10 items-center justify-center relative">
+    //                 <h1 className="text-9xl">404</h1>
+    //                 <p className="text-2xl">Oops... Something went wrong, maybe you inputted the wrong UID, or the account was not public.</p>
+    //                 <Link href="/" className="">
+    //                     <button className="border-text border-2 hover:bg-bg-dark transition-all px-4 p-2 rounded-xl">Back To Home</button>
+    //                 </Link>
+    //                 <div className="absolute bottom-0 w-full">
+    //                     <Footer />
+    //                 </div>
+    //             </main>
+    //         </>
+    //     );
+    // }
 
     return (
         <>
