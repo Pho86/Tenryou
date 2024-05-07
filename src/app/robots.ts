26b2cdbef030762c42aa8359fbf7dae3cc84fb6f
Philip Ho <91348256+Pho86@users.noreply.github.com>
2024-05-06 21:09:51 -0700

    fix robots route

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
             allow: '/',
             disallow: '/private/',
         },
-        sitemap: `${process.env.VERCEL_URL}/sitemap.xml`,
+        sitemap: `https://tenryou.live/sitemap.xml`,
     }
 }
