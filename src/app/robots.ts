63540358c51c1e57e64b92d12d23e190b5ead03a
Philip Ho <91348256+Pho86@users.noreply.github.com>
2024-05-12 10:12:22 -0700

    update sitemap url

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
             allow: '/',
             disallow: '/private/',
         },
-        sitemap: `https://tenryou.live/sitemap.xml`,
+        sitemap: `https://www.tenryou.live/sitemap.xml`,
     }
 }
