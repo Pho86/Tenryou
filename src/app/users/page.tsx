bf06d65c2c7cf3df5a6464cd1d3fc75443e0b602
Philip <philip03ho@gmail.com>
2025-01-10 19:22:25 -0800

    revamp user search page design

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -1,31 +1,59 @@
-"use client"
-import { useState } from "react"
+"use client";
+import { useState } from "react";
 import Link from "next/link";
 export default function UserPage() {
-    const [user, setUser] = useState({
-        uid: '' as string
-    });
-    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setUser({ ...user, [event.target.name]: event.target.value });
-    };
-    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault()
-    }
+  const [user, setUser] = useState({
+    uid: "" as string,
+  });
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setUser({ ...user, [event.target.name]: event.target.value });
+  };
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
 
-    return (
-        <>
-            <div className="flex flex-col gap-4 justify-center items-center mt-64">
-                <p className="text-center">Make sure the profile settings in the game have &quot;Show character details&quot; enabled, otherwise the API will not be able to see profile details. When an update is near, API no longer works.</p>
-                <p className="text-center">Example: 600020272</p>
-                <form className="flex justify-center items-center h-full w-full gap-2" onSubmit={handleSubmit} >
-                    <input type="number" name="uid" required value={user.uid} placeholder="Enter UID..." className="p-2 rounded-xl" onChange={handleChange} />
-                    <Link href={`/users/${user.uid}`}>
-                        <button className={`hover:bg-bg-dark p-2 rounded-xl transition-all border-2`} type="submit">Search</button>
-                    </Link>
-                </form>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className="flex flex-col gap-4 justify-center items-center mt-64">
+        <div className="grid md:grid-cols-2">
+          <div className="text-left text-pretty flex flex-col gap-2">
+            <h1 className="text-4xl text-primary text-pretty">
+              Your Genshin Builds and Team Cards
+            </h1>
+            <p className="text-md">
+              Make sure the profile settings in the game have &quot;Show
+              character details&quot; enabled, otherwise the API will not be
+              able to see profile details. When an update for Genshin happens,
+              the API does not work anymore.
+            </p>
+            <p className="">
+              Example: <strong>600020272</strong>
+            </p>
+          </div>
+          <form
+            className="flex justify-center items-center h-full w-full gap-2"
+            onSubmit={handleSubmit}
+          >
+            <input
+              type="number"
+              name="uid"
+              required
+              value={user.uid}
+              placeholder="Enter UID..."
+              className="p-2 rounded-xl"
+              onChange={handleChange}
+            />
+            <Link href={`/users/${user.uid}`}>
+              <button
+                className={`hover:bg-bg-dark p-2 rounded-xl transition-all border-2`}
+                type="submit"
+              >
+                Search
+              </button>
+            </Link>
+          </form>
+        </div>
+      </div>
+    </>
+  );
 }
-
-
